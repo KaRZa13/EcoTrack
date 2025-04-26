772a4c1d97f0c7f3601298de2465c69e958f4ad5
Colin MORLION <morlioncolin@gmail.com>
2025-04-26 20:46:29 +0200

    2025/04/26 - Visuel SignIn-Signup

--- pages/register.vue
+++ pages/register.vue
@@ -1,32 +1,86 @@
 <template>
-  <div class="w-screen h-screen flex items-center justify-center bg-white1 ">
-    <div class="max-w-md mx-auto mt-10 p-6 bg-white1 shadow-md rounded-lg">
-      <div class="flex flex-col items-center justify-center mb-4">
-        <AddPerson class="w-10 h-10" />
-        <h1 class="text-2xl font-bold text-center mb-6">Sign Up</h1>
+  <div class="w-screen h-screen flex items-center justify-center bg-white1  ">
+    <!-- Logo Ecotrack -->
+    <div class="absolute top-5 left-5">
+      <SvgMainLogo class="w-60 sm:w-80 md:w-100" />
+    </div>
+    <!-- Formulaire d'inscription -->
+    <div class="w-full rounded-lg mt-10 p-6 flex flex-col justify-center items-center md:flex-row gap-8 md:gap-12 max-w-4xl ">
+      <div class="max-w-xl mt-10 p-6 bg-white1 w-full rounded-lg">
+        <div class="p-8 items-center rounded-2xl flex flex-col text-3xl font-bold gap-6 flex-1 ">
+          <div class="flex self-start items-center gap-4">
+            <img src="../public/img/logocalculate.png" alt="Calculate">
+            <p>Calculate</p>
+          </div>
+          <div class="flex self-start items-center gap-4">
+            <img src="../public/img/logotracker.png" alt="Tracker">
+            <p>Track</p>
+          </div>
+          <div class="flex self-start items-center gap-4">
+            <img src="../public/img/logoreduce.png" alt="Reduce">
+            <p>Reduce</p>
+          </div>
+        </div>
       </div>
-      <form @submit.prevent="signUp" class="space-y-4">
-        <div class="form-group">
-          <input v-model="email" type="email" placeholder="Email" required
-            class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
+      <div class="max-w-xl mx-auto mt-10 p-6 bg-white1 border border-gray-300 w-full rounded-lg">
+        <div class="flex flex-col items-center justify-center mb-4">
+          <AddPerson class="w-20 h-20" />
+
         </div>
-        <div class="form-group">
-          <input v-model="password" type="password" placeholder="Mot de passe" required
-            class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
-        </div>
-        <div class="form-group">
-          <input v-model="confirmPassword" type="password" placeholder="Confirmez le mot de passe" required
-            class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
-        </div>
-        <button type="submit" class="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition">
-          S'inscrire
-        </button>
-        <p v-if="errorMsg" class="text-red-500 text-sm mt-2">{{ errorMsg }}</p>
-        <p v-if="successMsg" class="text-green-500 text-sm mt-2">{{ successMsg }}</p>
-      </form>
-      <NuxtLink to="/login" class="block text-center text-blue-500 mt-4 hover:underline">
-        Vous avez déjà un compte ? Connectez-vous
-      </NuxtLink>
+        <form @submit.prevent="signUp" class="space-y-4 pt-4">
+          <div class="form-group">
+            Email
+            <input v-model="email" type="email" placeholder="Email" required
+              class="w-full mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-gray-300" />
+          </div>
+          <div class="form-group pt-2">
+            Password
+            <input v-model="password" type="password" placeholder="Password" required
+              class="w-full mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-gray-300" />
+          </div>
+          <div class="form-group pt-2">
+            Password confirmation
+            <input v-model="confirmPassword" type="password" placeholder="Password" required
+              class="w-full mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-gray-300" />
+          </div>
+          <div class="form-group pt-2">
+            Code entreprise
+            <input v-model="codeEntreprise" type="entreprise" placeholder="XXXXXXXXX" required
+              class="w-full mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-gray-300" />
+          </div>
+          
+          <div class="inline-flex items-center py-2">
+            <label class="flex items-center cursor-pointer relative" for="check-with-link">
+              <input type="checkbox" required
+                class="peer h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-slate-300 checked:bg-black checked:border-slate-800"
+                id="check-with-link" />
+              <span
+                class="absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
+                  stroke="currentColor" stroke-width="1">
+                  <path fill-rule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clip-rule="evenodd"></path>
+                </svg>
+              </span>
+            </label>
+            <label class="select-none cursor-pointer ml-2 text-slate-800 text-sm" for="check-with-link">
+              <p>
+                I agree with the <span class="underlined">terms and conditions.</span>
+              </p>
+            </label>
+          </div>
+          <button type="submit"
+            class="w-full bg-black text-white py-2 rounded-md border cursor-pointer hover:bg-white1 hover:border-gray-600 hover:text-black transition">
+            Sign up
+          </button>
+          <p v-if="errorMsg" class="text-red-500 text-sm mt-2">{{ errorMsg }}</p>
+          <p v-if="successMsg" class="text-green-500 text-sm mt-2">{{ successMsg }}</p>
+        </form>
+        <NuxtLink to="/login" class="block text-center text-gray-500 mt-4">
+          Already have an account? <span class="underline">Sign in</span>
+        </NuxtLink>
+      </div>
     </div>
   </div>
 </template>
@@ -34,11 +88,12 @@
 <script setup lang="ts">
 import AddPerson from '../components/svg/AddPerson.vue'
 
-const client = useSupabaseClient()
-const router = useRouter()
 
+const client = useSupabaseClient()
 const email = ref<string>("")
 const password = ref<string>("")
+const codeEntreprise = ref<string>("")
+
 const confirmPassword = ref<string>("")
 const errorMsg = ref<string>("")
 const successMsg = ref<string>("")
@@ -57,13 +112,10 @@ const signUp = async () => {
       email: email.value,
       password: password.value,
     })
-    if (DataTransfer,error) {
+    if (error) {
       errorMsg.value = error.message
     } else {
       successMsg.value = "Vérifiez votre email pour le lien de confirmation."
-      nextTick(() => {
-        router.push("/login")
-      })
     }
   } catch (error: any) {
     errorMsg.value = error.message || "Une erreur est survenue."

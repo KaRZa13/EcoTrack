8b79b6e9d3ada22d0eacfa6792c4a16660896dbe
KaRZa13 <rafael.francisco.muro@gmail.com>
2025-04-27 00:08:14 +0200

    2025/04/26 - TODO: form(urgent)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
 import tailwindcss from "@tailwindcss/vite"
-// https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxtjs/supabase'],
+  modules: [
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxtjs/supabase'
+  ],
   css: ['~/assets/css/main.css'],
   supabase: {
     redirect: true,

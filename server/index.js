6765e0cc4670968ef2c05b61864f61891ec44bcc
unknown <rafael.muro@proton.me>
2025-06-10 15:07:34 +0200

    m

--- server/index.js
+++ server/index.js
@@ -257,6 +257,6 @@ app.post('/register', async (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`The server is running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`The server is running on http://0.0.0.0:${port}`);
 });
